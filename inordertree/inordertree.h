2a2957899f77255aae395033c21faf19c53ad0ee
xc1129 <xc@xc-PC.(none)>
2012-05-28 22:16:55 +0800

    modify the class defination

--- inordertree/inordertree.h
+++ inordertree/inordertree.h
@@ -1,19 +1,29 @@
 #ifndef inordertree
 #define inordertree
 
+template <class T>
 struct treeNode
 {
-    int data;
+    T data;
     treeNode *left;
     treeNode *right;
     treeNode *parent;
 };
 
+template <class T>
 class inordertree
 {
     public:
-
+        inordertree();
+        ~inordertree();
+        int size() const;
+        int height() const;
+        void midOrder() const;
+        void preOrder() const;
+        void postOrder() const;
+        void createTree() const;
     private:
+        treeNode<T> *root;
 }
 
 #endif
