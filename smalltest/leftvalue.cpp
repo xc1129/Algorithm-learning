7ca88f7fe24177ee2c70e6a607941bac57790bb5
xc1129 <xc@xc-PC.(none)>
2012-05-31 20:02:40 +0800

    small tests

---
+++ smalltest/leftvalue.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author:  Spirit_xc
+ * Description:
+ *
+ *
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+char &get_val(string &str, string::size_type ix)
+{
+    return str[ix];
+}
+
+int main()
+{
+    string s("a value");
+    cout << s <<endl;
+    get_val(s,0)='A';
+    cout << s <<endl;
+    return 0;
+}
+
+
+
